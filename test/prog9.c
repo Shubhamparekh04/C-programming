6228daa39e490a0c467cf194bce16a2d421c22db
Shubhamparekh04 <shubhamparekh04@gmail.com>
2024-07-25 23:59:21 +0530

    Done

--- test/prog9.c
+++ test/prog9.c
@@ -54,3 +54,147 @@ void main(){
 
 }
 */
+
+
+
+/*
+// user define function for string length
+
+int stringlength(char[]);
+#include<stdio.h>
+
+int stringlength(char x[]){
+    int i=0,ct=0;
+    while(x[i]!='\0'){
+        ct++;
+        i++;
+    }
+    return ct;
+}
+
+void main(){
+    int len;
+    char name[20];
+    printf("Enter Name : ");
+    gets(name);
+    len = stringlength(name);
+
+    printf("Length is : %d",len);
+}
+*/
+
+
+/*
+//uppercase character string to lowercase
+//<string.h> = strlwn();
+
+
+#include<stdio.h>
+#include<string.h>
+void main(){
+char name[20]="SHUBHAM";
+printf("%s",strlwr(name));
+}
+*/
+
+/*
+//UPPERCASE to lowercase without strlwr() function
+
+#include<stdio.h>
+void main(){
+    int i=0;
+    char x[20]="A+nN*";
+
+    while(x[i]!='\0'){
+        if(x[i]>='A' && x[i]<='Z'){
+          x[i] = x[i]+32;
+        }
+        i++;
+    }
+
+    printf("%s",x);
+
+}
+*/
+
+/*
+//Concat 2 strings
+
+//1st Method
+#include<stdio.h>
+#include<string.h>
+void main(){
+    int i=0,len;
+    char s1[20]="Hello", s2[20]="World";
+
+    len = strlen(s1);
+
+    while(s2[i]!='\0'){
+        s1[len+i]=s2[i];
+        i++;
+    }
+    printf("%s",s1);
+}
+*/
+
+/*
+//2nd Method
+#include<stdio.h>
+#include<string.h>
+void main(){
+    int l1,l2;
+    char s1[20]="Hello", s2[20]="World";
+
+    l1=strlen(s1);
+    l2=strlen(s2);
+
+    for(int i=0;i<=l2;i++){
+        s1[l1+i]=s2[i];
+    }
+
+    printf("%s",s1);
+
+}
+*/
+
+/*
+// Structure
+
+//Declare structure 
+struct emp{
+    int eno; // 4 bytes
+    char ename[20]; // 20 bytes
+    float esal; // 4 bytes
+};
+
+#include<stdio.h>
+void main(){
+
+//Declare variable (e) with structure data type
+    struct emp e;
+
+//printing size of structure occupied in memory
+    printf("Size of : %d\n",sizeof(e)); // 28 bytes
+    printf("Size of : %d",sizeof(struct emp)); // 28 bytes
+}
+*/
+
+/*
+//Assign value & acces value to structure elements
+
+// global declaration
+struct emp{
+    int eno; // 4 bytes
+    char ename[20]; // 20 bytes
+    float esal; // 4 bytes
+};
+
+#include<stdio.h>
+void main(){
+
+    struct emp e = {1,"shubham",100000};
+    e.esal = 200000;
+    printf("Eno : %d\nEname : %s\nsalary : %.2f",e.eno,e.ename,e.esal);
+
+}
+*/
