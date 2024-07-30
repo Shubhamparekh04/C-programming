73af6b2b0533411a4942e9fd36601472975878b8
Shubhamparekh04 <shubhamparekh04@gmail.com>
2024-07-31 00:20:30 +0530

    Pattern

--- 6.Looping/Lab Work 6.3/prog4.c
+++ 6.Looping/Lab Work 6.3/prog4.c
@@ -7,43 +7,39 @@
 */
 
 #include <stdio.h>
-
 void main()
 {
     for (int i = 1; i <= 5; i++)
     {
-        int sum = 0,a = 1;
+        int sum = 0, a = 1;
         for (int j = 1; j <= i; j++)
         {
             if (j == 1)
             {
                 printf("1 ");
             }
+            else if (i == 2 && j != 1)
+            {
+                sum = a + 1;
+                printf("%d ", sum);
+            }
+            else if (i == 3 && j != 1)
+            {
+                sum = a + 2;
+                printf("%d ", sum);
+                a = sum;
+            }
+            else if (i == 4 && j != 1)
+            {
+                sum = a + 5;
+                printf("%d ", sum);
+                a = sum;
+            }
             else
             {
-                if (i == 2 && j != 1)
-                {
-                    sum =  a + 1;
-                    printf("%d ", sum);
-                }
-                else if (i == 3 && j != 1)
-                {
-                    sum = a + 2;
-                    printf("%d ", sum);
-                    a = sum;
-                }
-                else if (i == 4 && j != 1)
-                {
-                    sum = a + 5;
-                    printf("%d ", sum);
-                    a = sum;
-                }
-                else if (i == 5 && j != 1)
-                {
-                    sum = a + 16;
-                    printf("%d ", sum);
-                    a = sum;
-                }
+                sum = a + 16;
+                printf("%d ", sum);
+                a = sum;
             }
         }
         printf("\n");
--- 6.Looping/Lab Work 6.3/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-                // else if (i == 3)
-                // {
-                //     sum = sum + a + 1;
-                //     printf("%d ", sum);
-                // }
